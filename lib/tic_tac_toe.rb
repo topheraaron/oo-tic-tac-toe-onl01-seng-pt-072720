ebf48d8ee7cd508e5920b69593afdefbb2ee736a
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-19 22:54:50 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -59,4 +59,17 @@ class TicTacToe
   end
   return player
   end
+
+  def turn
+  puts "Please choose a number 1-9:"
+  user_input = gets.chomp
+  index = input_to_index(user_input)
+  if valid_move?(index)
+    player_token = current_player
+    move(index, player_token)
+    display_board
+  else
+    turn
+    end
+  end
 end
