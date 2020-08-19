aef9f2b10fc61c36e4ae9a90f69452fb4908070e
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-19 22:19:16 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -16,7 +16,10 @@ class TicTacToe
   end
 
   def display_board
-    board = ["X", "X", "X", "X", "O", "O", "X", "O", "O"]
-    display_board(board)
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 end
