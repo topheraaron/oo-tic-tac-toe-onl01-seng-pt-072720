7da1f33a5318e887732ee5819191fdd397f98ed3
Christopher Arias <chrstphr.aaron@gmail.com>
2020-08-19 23:06:17 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -125,4 +125,16 @@ class TicTacToe
       end
     end
   end
+
+  def play
+    until over? == true
+      turn
+    end
+
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cat's Game!"
+    end
+  end
 end
